933328c910a5579d28ea0488e88ab5b091472707
Vitor <vitorricardosilvabarbosa@gmail.com>
2024-08-23 14:22:10 -0300

    Commit inicial * Criação de projeto Java para estudos de `Programação Orientada a Objetos` * Criação de classes e instâncias de objetos

---
+++ src/Caneta.java
@@ -0,0 +1,28 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga + "% cheia");
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    void rabiscar() {
+        if (this.tampada) {
+            System.out.println("Estou tampada e não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
+    void tampar(){
+        this.tampada = true;
+    }
+    void destampar(){
+        this.tampada = false;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Caneta azul = new Caneta();
+        azul.modelo = "BIC";
+        azul.cor = "Azul";
+        azul.ponta = 0.5f;
+        azul.carga = 90;
+        azul.tampada = true;
+
+        Caneta preta = new Caneta();
+        preta.modelo = "Pilot";
+        preta.cor = "Preta";
+        preta.ponta = 0.8f;
+        preta.carga = 30;
+        preta.tampada = false;
+
+        azul.status();
+        azul.rabiscar();
+        preta.status();
+        preta.rabiscar();
+
+    }
+}
