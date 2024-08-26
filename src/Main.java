1d05be58a6e5b72833d2ff67e9478972bd7b91d3
Vitor <vitorricardosilvabarbosa@gmail.com>
2024-08-26 11:19:53 -0300

    Aula de agregação com exemplo de luta * Aplicando agregação simples * Uso de número aleatório para definir lutadores e resultado das lutas

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import aula07.ultraEmojiCombat.Luta;
 import aula07.ultraEmojiCombat.Lutador;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -58,19 +63,32 @@ public class Main {
 //        controle.desligar();
 //        controle.abrirMenu();
 
-        Lutador l[] = new Lutador[6];
+//        Lista de lutadores
+        Lutador[] l = new Lutador[6];
         l[0] = new Lutador("Pretty Boy","França",31,1.75f,68.9f,11,2,1);
-        l[0].apresentarLutador();
         l[1] = new Lutador("Putscript","Brasil",29,1.68f,57.8f,14,2,3);
-        l[1].apresentarLutador();
         l[2] = new Lutador("Snapshadow","EUA",35,1.65f,80.9f,12,2,1);
-        l[2].apresentarLutador();
         l[3] = new Lutador("Dead Code","Austrália",28,1.93f,81.6f,13,0,2);
-        l[3].apresentarLutador();
         l[4] = new Lutador("Ufocolob","Brasil",37,1.7f,119.3f,5,4,3);
-        l[4].apresentarLutador();
         l[5] = new Lutador("Nerdaart","EUA",30,1.81f,105.7f,12,2,4);
-        l[5].apresentarLutador();
 
+//        Inicar uma luta
+        Random aleatorio = new Random();
+        List<Integer> listaPar = new ArrayList<>(){{
+            add(0);
+            add(2);
+            add(4);
+        }};
+        List<Integer> listaImpar = new ArrayList<>(){{
+            add(1);
+            add(3);
+            add(5);
+        }};
+        Lutador l1 = l[listaPar.get(aleatorio.nextInt(3))];
+        Lutador l2 = l[listaImpar.get(aleatorio.nextInt(3))];
+
+        Luta uec01 = new Luta();
+        uec01.marcarLuta(l1, l2);
+        uec01.lutar(l1,l2);
     }
 }
--- src/aula07/ultraEmojiCombat/Luta.java
+++ src/aula07/ultraEmojiCombat/Luta.java
@@ -1,11 +1,100 @@
 package aula07.ultraEmojiCombat;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Luta {
     private Lutador desafiado;
     private Lutador desafiante;
     private int rounds;
     private boolean aprovada;
 
-    void marcarLuta(){}
-    void lutar(){}
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+
+    public void marcarLuta(Lutador desafiante,Lutador desafiado){
+        if(desafiante != desafiado &&
+           Objects.equals(desafiante.getCategoria(), desafiado.getCategoria())){
+            this.setAprovada(true);
+            this.desafiante = desafiante;
+            this.desafiado = desafiado;
+        } else {
+            this.setAprovada(false);
+            this.desafiante = null;
+            this.desafiado = null;
+        }
+    }
+    public void lutar(Lutador desafiante,Lutador desafiado){
+        if(this.isAprovada()) {
+        desafiado.apresentarLutador();
+        desafiante.apresentarLutador();
+
+        Random aleatorio = new Random();
+        int resultado = aleatorio.nextInt(3);
+        switch (resultado){
+            case 0:
+                System.out.println("Empatou!");
+                desafiante.empatarLuta();
+                desafiado.empatarLuta();
+                break;
+            case 1:
+                System.out.println(desafiante.getNome() + " ganhou!");
+                desafiante.ganharLuta();
+                desafiado.perderLuta();
+                System.out.println("O RESULTADO É O SEGUINTE:");
+                System.out.println("AGORA TEMOS O NOVO VENCEDOR");
+                System.out.println("E O NOME DELE É......");
+                System.out.println(desafiante.getNome().toUpperCase());
+                desafiante.status();
+                break;
+            case 2:
+                System.out.println(desafiado.getNome() + " ganhou!");
+                desafiante.perderLuta();
+                desafiado.ganharLuta();
+                System.out.println("O RESULTADO É O SEGUINTE:");
+                System.out.println("AGORA TEMOS O NOVO VENCEDOR");
+                System.out.println("E O NOME DELE É......");
+                System.out.println(desafiado.getNome().toUpperCase());
+                desafiado.status();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + resultado);
+        }
+        } else {
+            System.out.println("Luta não pode acontecer.");
+        }
+
+    }
+    public void apresentarResultado(Lutador desafiante,Lutador desafiado){
+
+    }
 }
