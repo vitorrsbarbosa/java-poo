49af79e08363aa4101595919bf940091fe347331
vitor <vitorricardosilvabarbosa@gmail.com>
2024-08-25 21:23:32 -0300

    Preparação para relacionamento de agregação para próxima aula

---
+++ src/aula07/ultraEmojiCombat/Luta.java
@@ -0,0 +1,11 @@
+package aula07.ultraEmojiCombat;
+
+public class Luta {
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    void marcarLuta(){}
+    void lutar(){}
+}
